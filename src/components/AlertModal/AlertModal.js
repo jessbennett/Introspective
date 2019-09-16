9212d92e079b68bd80bb936249298443fbcf487d
bennettj1 <jessica.bennett@hmhco.com>
2019-09-16 00:02:08 -0400

    added eslint and node modules

---
+++ src/components/AlertModal/AlertModal.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line react/jsx-tag-spacing
+import React from "react";
+import PropTypes from "prop-types";
+
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import { withStyles } from "@material-ui/core/styles";
+
+const AlertModal = props => (
+  <Dialog aria-labelledby="alert-dialog-title"></Dialog>
+);
