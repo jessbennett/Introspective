b030aebb5a910c26fa5cf90727b9e756ae46ab0a
bennettj1 <jessica.bennett@hmhco.com>
2019-11-05 11:30:55 -0500

    added more labels to form

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -11,23 +17,54 @@ const useStyles = makeStyles(theme => ({
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     width: 200
+  },
+  formControl: {
+    margin: theme.spacing(3)
   }
 }));
 
 export default function InputForm() {
   const classes = useStyles();
+  const [value, setValue] = React.useState("Yes");
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
 
   return (
     <form className={classes.container} noValidate autoComplete="off">
       <div>
         <TextField
-          multiline
-          id="outlined-basic"
+          id="standard-name"
+          label="Your Name"
           className={classes.textField}
-          label="Outlined"
           margin="normal"
           variant="outlined"
         />
+        <TextField
+          multiline
+          id="team-name"
+          className={classes.textField}
+          label="Team Name"
+          margin="normal"
+          variant="outlined"
+        />
+        <Grid>
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormLabel component="legend">
+              Would you like to have anonymous users?
+            </FormLabel>
+            <RadioGroup
+              aria-label="user selection"
+              name="selection1"
+              value={value}
+              onChange={handleChange}
+            >
+              <FormControlLabel value="Yes" control={<Radio />} label="Yes" />
+              <FormControlLabel value="No" control={<Radio />} label="No" />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
       </div>
     </form>
   );
