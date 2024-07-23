14e79309424309cec00dcbb63aa08e17c314ca4c
Jessica Bennett <Jessbennett924@gmail.com>
2024-07-23 19:26:54 -0400

    added dick for jest

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
